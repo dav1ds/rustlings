520c3832487943cfd5fac5d83b300796782cfae7
dav1ds <david.sierp@gmail.com>
2023-06-03 08:33:32 +0200

    error handlings  5

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,7 +1,6 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,8 +13,12 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        match value {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+            v if v > 0 => Ok(PositiveNonzeroInteger(value as u64)),
+            v if v<0 => Err(CreationError::Negative),
+            _ =>Err(CreationError::Zero), 
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,28 +39,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        scores.insert(
-            team_1_name.to_string(),
-            Team {
-                name: team_1_name,
-                goals_scored: team_1_score,
-                goals_conceded: team_2_score,
-            },
-        );
 
-        scores.insert(
-            team_2_name.to_string(),
-            Team {
-                name: team_2_name,
-                goals_scored: team_2_score,
-                goals_conceded: team_1_score,
-            },
-        );
-        scores.entry(team_1_name.to_string()).or_insert(Team {
-            name: team_1_name,
-            goals_scored: team_2_score,
-            goals_conceded: team_1_score,
-        });
+// Update the scores table for team1
+            let team1_entry = scores.entry(team_1_name.clone()).or_insert(Team {
+                name: team_1_name.clone(),
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+            team1_entry.goals_scored += team_1_score;
+            team1_entry.goals_conceded += team_2_score;
+
+            // Update the scores table for team2
+            let team2_entry = scores.entry(team_2_name.clone()).or_insert(Team {
+                name: team_2_name.clone(),
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+            team2_entry.goals_scored += team_2_score;
+            team2_entry.goals_conceded += team_1_score;
+
+
     }
     scores
 }
